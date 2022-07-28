7bcf49568fda762ffe4de846b571a43e09a75f78
Roge <roge.vpd@gmail.com>
2022-07-28 17:55:00 +0800

    save

---
+++ purchasepdf.php
@@ -0,0 +1,29 @@
+<?php
+require('fpdf184/fpdf.php');
+$conn=mysqli_connect('localhost','root','','growth');
+if($conn->connect_error){
+    die("Error in DB connection:".$conn->connect_errno.":".$conn->_error);
+}
+$select = "SELECT * FROM purchase_order ORDER BY id";
+$result = $conn->query($select);
+$pdf = new FPDF();
+$pdf-> AddPage();
+$pdf->SetFont('Arial','',12);
+$pdf->SetFillColor(77, 153, 0);
+$pdf->Cell (190 ,5, 'PURCHASE ORDER','B', 0,'C',true);
+
+
+while($row=$result->fetch_object()){
+
+    $id=$row->id;
+    $purchase_date=$row->purchase_date;
+    $expected_date=$row->expected_date;
+    $item=$row->item;
+    $quantity=$row->quantity;
+    $price=$row->price;
+    $amount_total=$row->amount_total;
+
+}
+$pdf->Cell(190,10,'LOGO HERE',0,0,'C');
+$pdf ->Output();
+?>
