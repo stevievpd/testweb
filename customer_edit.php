0b0ace21e57bc8e01f249115d624469dfff8d8f9
sam-vpd <sammy.vpd@gmail.com>
2022-08-08 14:26:56 +0800

    customer crud done

--- customer_edit.php
+++ customer_edit.php
@@ -1,7 +1,7 @@
-<?php
+<!-- <?php
 	include 'includes/session.php';
 
-	if(isset($_POST['add'])){
+	if(isset($_POST['edit'])){
 		$customer_firstname = $_POST['customer_firstname'];
 		$customer_lastname = $_POST['customer_lastname'];
 		$customer_address = $_POST['customer_address'];
@@ -29,6 +29,33 @@
 	}
 	else{
 		$_SESSION['error'] = 'Fill up add form first';
+	}
+
+	header('location: customer.php');
+?> -->
+
+
+
+<?php
+	include 'includes/session.php';
+
+	if(isset($_POST['edit'])){
+		$id = $_POST['id'];
+		$firstname = $_POST['edit_customer_first_name'];
+		$lastname = $_POST['edit_customer_last_name'];
+		$contact = $_POST['edit_customer_contact'];
+		$address = $_POST['edit_customer_address'];
+		
+		$sql = "UPDATE customer SET customer_firstname = '$firstname', customer_lastname = '$lastname', customer_contact_info = '$contact', customer_address = '$address' WHERE id = '$id'";
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Product updated successfully';
+		}
+		else{
+			$_SESSION['error'] = $conn->error;
+		}
+	}
+	else{
+		$_SESSION['error'] = 'Select products to edit first';
 	}
 
 	header('location: customer.php');
