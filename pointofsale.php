6abe20f07ce6be772b5494a44a9ff1ec30fe4b43
Roge <roge.vpd@gmail.com>
2022-08-12 18:02:12 +0800

    updated pos

--- pointofsale.php
+++ pointofsale.php
@@ -12,7 +12,7 @@
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <h1>
-                    Dashboard
+                    Point Of Sales
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
